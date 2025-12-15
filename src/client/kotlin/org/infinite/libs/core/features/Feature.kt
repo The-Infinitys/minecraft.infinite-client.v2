182049a19671f7df403b64d68fe5f802a2755ebe
The-Infinitys <the.infinity.s.infinity@gmail.com>
2025-12-15 22:49:34 +0900

    TODO: complete translations features

--- src/client/kotlin/org/infinite/libs/core/features/Feature.kt
+++ src/client/kotlin/org/infinite/libs/core/features/Feature.kt
@@ -1,3 +1,10 @@
 package org.infinite.libs.core.features
 
-open class Feature
+open class Feature {
+    open val properties: HashMap<String, Property> = HashMap.newHashMap(0)
+
+    init {
+        val kClass = this::class
+        val qualifiedName: String? = kClass.qualifiedName
+    }
+}
